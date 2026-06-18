7cbcb690b06230c615c5a59e037985e5eda83756
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-06-18 02:20:22 -0300

    refatoraçoes

--- src/main/java/com/ccp/topic/consumer/pubsub/pull/CcpMessageReceiver.java
+++ src/main/java/com/ccp/topic/consumer/pubsub/pull/CcpMessageReceiver.java
@@ -1,10 +1,10 @@
 package com.ccp.topic.consumer.pubsub.pull;
 
-import com.ccp.constantes.CcpOtherConstants;
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.decorators.CcpJsonRepresentation.CcpJsonFieldName;
 import com.ccp.especifications.db.utils.entity.CcpEntity;
 import com.ccp.business.CcpBusiness;
+import com.ccp.constants.CcpOtherConstants;
 import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import com.google.cloud.pubsub.v1.MessageReceiver;
 import com.google.protobuf.ByteString;
